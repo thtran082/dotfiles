8239186c82fab05b4dd4975d228796e2f4ee20fc
Huy Tran <yuhnart@Huys-MacBook-Pro.local>
2024-07-23 21:45:34 +0700

    chore: lazyvim stuffs

---
+++ dot_config/nvim/lua/plugins/angular-switcher.lua
@@ -0,0 +1,21 @@
+return {
+  "softoika/ngswitcher.vim",
+  keys = {
+    {
+      "<leader>nu",
+      ":NgSwitchTS<CR>",
+    },
+    {
+      "<leader>ni",
+      ":NgSwitchCSS<CR>",
+    },
+    {
+      "<leader>no",
+      ":NgSwitchHTML<CR>",
+    },
+    {
+      "<leader>np",
+      ":NgSwitchSpec<CR>",
+    },
+  },
+}
--- dot_config/nvim/lua/plugins/neotree.lua
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -2,15 +2,15 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      event_handlers = {
-        {
-          event = "file_opened",
-          handler = function()
-            --auto close
-            require("neo-tree.command").execute({ action = "close" })
-          end,
-        },
-      },
+      -- event_handlers = {
+      --   {
+      --     event = "file_opened",
+      --     handler = function()
+      --       --auto close
+      --       require("neo-tree.command").execute({ action = "close" })
+      --     end,
+      --   },
+      -- },
       filesystem = {
         window = {
           mappings = {
